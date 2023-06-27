0a6748c36a4d3e9617fedf0890084ae60fcb55f4
JColeJ <jordzcs@gmail.com>
2023-06-27 18:59:10 -0400

    Initial Commit

---
+++ src/main/java/com/calculator/supreme/CalculatorSupremeApiApplication.java
@@ -0,0 +1,13 @@
+package com.calculator.supreme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorSupremeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorSupremeApiApplication.class, args);
+	}
+
+}
